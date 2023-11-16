804906b919728d603f81cc6cee15530347a85926
viniskttt <128101413+viniskttt@users.noreply.github.com>
2023-11-16 20:23:23 -0300

    Update main.js
    dfrr

--- main.js
+++ main.js
@@ -2,4 +2,36 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const litaDeTeclas = documente.querySelectorAll(' .tecla');
+
+//para
+
+for  (let contador = 0; contaor  <listaDeTeclas.length;contador++){
+
+    const tecla = listaDeTeclas[contaor];
+
+    cont instrumento = tecla.classLit[1];
+
+    const idAudio = '#som_${instrumento} ´; //template string
+
+    tecla.onclick = function () {
+        tocaSomPom(idAudio);
+}
+
+tecla.onkeydown = function (evento)  {
+
+    console.log(evento.cod==)
+
+    if(evento.cod = 'Space'){
+
+        tecla.classLit.add('ativa');
+}
+
+
+}
+
+tecla.onkeydown = function(){
+    tecla.classLit.remove('ativa');
+}
+
+}
