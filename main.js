3559a369603d84a11dea16047e3a1849bf8e9c78
viniskttt <128101413+viniskttt@users.noreply.github.com>
2023-11-30 20:40:24 -0300

    Add files via upload
    jnh

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const litaDeTeclas = documente.querySelectorAll(' .tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-for  (let contador = 0; contaor  <listaDeTeclas.length;contador++){
-
-    const tecla = listaDeTeclas[contaor];
-
-    cont instrumento = tecla.classLit[1];
-
-    const idAudio = '#som_${instrumento} ´; //template string
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
-        tocaSomPom(idAudio);
-}
-
-tecla.onkeydown = function (evento)  {
-
-    console.log(evento.cod==)
-
-    if(evento.cod = 'Space'){
-
-        tecla.classLit.add('ativa');
-}
-
+        tocaSom(idAudio);
+    }
 
 }
 
-tecla.onkeydown = function(){
-    tecla.classLit.remove('ativa');
+ tecla.onkeydown = function (evento) {
+
+if (evento.code === 'Space'||evento.code === 'Enter') {
+
+    tecla.classList.add('ativa');
 }
 
-}
+ }
+
+
+
+ tecla.onkeydoup = function () {
+    tecla.classList.remove('ativa');
+
+
+ }
